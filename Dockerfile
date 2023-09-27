1eae2f205da03201831415ea0be886c9f88cba2d
Stein van Broekhoven <stein@aapjeisbaas.nl>
2023-09-27 12:54:59 +0200

    make magic

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM semaphoreui/semaphore:latest
+USER root
+RUN apk add bash build-base python3-dev krb5-dev libssh-dev libssh2-dev oniguruma-dev
+COPY requirements.txt .
+COPY requirements.yml .
+RUN apk add py3-pip git vim ; pip3 install -r requirements.txt ; ansible-galaxy collection install -r requirements.yml -p /usr/share/ansible/collections ;
+RUN apk add vim
+COPY pip-installer.sh .
+RUN ./pip-installer.sh
+#USER semaphore
---
+++ pip-installer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# pull in all requirements from the 
+pip install --upgrade pip
+find /usr/share/ansible/ -type f -iname '*requirements.txt' -exec cat "{}" \; | grep -ve 'python_version\|antsibull\|git\+\|docker-compose' | sed 's/#.*//g; s/>=.*//g; s/~=.*//g; s/==.*//g; s/>.*//g; s/<.*//g; s/[[:space:]]*$//g; s/\!=.*//g;  /^$/d'  | sort | uniq >> reqs.txt
+while read p; do python3 -c "import $p" || python3 -m pip show "$p" || echo "$p" >> requirements.txt ; done < reqs.txt
+pip3 install --debug -r requirements.txt --ignore-installed packaging PyYAML
+
