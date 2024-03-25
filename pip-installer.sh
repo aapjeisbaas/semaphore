2a02da3f27d344ff34f72899e36a9e6ca022b75a
Stein van Broekhoven <stein@aapjeisbaas.nl>
2024-03-25 14:19:24 +0100

    check if package is installable

--- pip-installer.sh
+++ pip-installer.sh
@@ -3,9 +3,27 @@
 pip install --upgrade pip
 find /usr/share/ansible/ -type f -iname '*requirements.txt' -exec cat "{}" \; | grep -ve 'python_version\|antsibull\|git\+\|docker-compose' | sed 's/#.*//g; s/>=.*//g; s/~=.*//g; s/==.*//g; s/>.*//g; s/<.*//g; s/[[:space:]]*$//g; s/\!=.*//g;  /^$/d'  | sort | uniq >> reqs.txt
 while read p; do python3 -c "import $p" || python3 -m pip show "$p" || echo "$p" >> requirements.txt ; done < reqs.txt
+
 # for debug purposes
 echo "Contents of requirements.txt:"
-cat reqs.txt
+cat requirements.txt
 echo "End of requirements.txt"
-pip3 install --debug -r requirements.txt --ignore-installed packaging PyYAML
+
+# check if packages are instalable
+for package in $(cat requirements.txt); do
+    pip install $package --dry-run
+    if [ $? -eq 0 ]; then
+        echo $package >> checked-requirements.txt
+    else
+        echo $package >> failed-requirements.txt
+    fi
+done
+
+# for debug purposes
+echo "Contents of failed-requirements.txt:"
+cat failed-requirements.txt
+echo "End of failed-requirements.txt"
+
+
+pip3 install --debug -r checked-requirements.txt --ignore-installed packaging PyYAML
 
