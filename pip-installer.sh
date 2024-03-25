ac079958e4cb17ee6fe9a58c941445e6e9828d56
Stein van Broekhoven <stein@aapjeisbaas.nl>
2024-03-25 13:51:20 +0100

    debug the pip installer

--- pip-installer.sh
+++ pip-installer.sh
@@ -3,5 +3,9 @@
 pip install --upgrade pip
 find /usr/share/ansible/ -type f -iname '*requirements.txt' -exec cat "{}" \; | grep -ve 'python_version\|antsibull\|git\+\|docker-compose' | sed 's/#.*//g; s/>=.*//g; s/~=.*//g; s/==.*//g; s/>.*//g; s/<.*//g; s/[[:space:]]*$//g; s/\!=.*//g;  /^$/d'  | sort | uniq >> reqs.txt
 while read p; do python3 -c "import $p" || python3 -m pip show "$p" || echo "$p" >> requirements.txt ; done < reqs.txt
+# for debug purposes
+echo "Contents of requirements.txt:"
+cat reqs.txt
+echo "End of requirements.txt"
 pip3 install --debug -r requirements.txt --ignore-installed packaging PyYAML
 
